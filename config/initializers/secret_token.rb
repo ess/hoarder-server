ddf8e6b8db120821e725aa591ff57665806e8dc7
Ess <pooster@gmail.com>
2011-07-06 00:32:06 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HoarderServer::Application.config.secret_token = 'b1d869d2b1fce0a91e7ff301e3a2bc80399ad3f66c395e94dacfe94d843d3d7111f66dc4c824fc2251c656909a75b5834954f17c97c055879d32f0b1b690290f'
